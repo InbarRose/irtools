bcb4650ec7a9c3eb90501583f031b987afbb4265
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2018-07-23 16:08:04 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from irtools import utils
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irtools",
+    version="1.0.0",
+    author="Inbar Rose",
+    author_email="inbar.rose1@gmail.com",
+    description="A package with useful tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/InbarRose/irtools",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
