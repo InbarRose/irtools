208c022d840dea338d7527b01b5bb9e37178ac55
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2018-07-31 16:55:38 +0300

    restful api improvement to logging add JsonRestObject convenience

--- irtools/kits/restful_api.py
+++ irtools/kits/restful_api.py
@@ -4,6 +4,7 @@ import json
 
 # irtools Imports
 from irtools import *
+from json_structure import JsonStructure
 
 # External Imports
 import requests
@@ -37,21 +38,22 @@ class RestfulAPI(object):
         '_log_dir_response',
     ]
     # default log paths
-    _cls_log_dir = log_dir
-    _cls_request_dir = log_dir + '/request'
-    _cls_response_dir = log_dir + '/response'
+    _cls_log_dir = log_dir  # you should override this in your inheritors
+    _cls_request_dir = _cls_log_dir + '/request'
+    _cls_response_dir = _cls_log_dir + '/response'
     # errors that should be ignored when converting JSON data
     _ignored_json_convert_error_messages = [
         'Extra data',
         'No JSON object could be decoded',
     ]
     # this file format is here so it can be extended easily
-    _format_log_file_path = '{directory}/{method}_{id}.txt'
+    _format_counter_id = '{id:03}'
+    _format_log_file_path = '{directory}/{transaction_id}.txt'
 
     def __init__(self, base_url, **kwargs):
         self.base_url = base_url
         self.name = kwargs.pop('name', 'rest')
-        self.log_rest_dir = kwargs.pop('log_rest_dir', os.path.join(log_dir, self.name))
+        self.log_rest_dir = kwargs.pop('log_rest_dir', os.path.join(self._cls_log_dir, self.name))
 
     @property
     def request_data_dir(self):
@@ -68,19 +70,24 @@ class RestfulAPI(object):
         kwargs.setdefault('_log_dir_response', self.response_data_dir)
 
     @classmethod
-    def _get_log_request_file_path(cls, method, directory, transact_id):
+    def _get_log_request_file_path(cls, directory, transact_id):
         """creates the final file path for request logs"""
-        return cls._format_log_file_path.format(directory=directory, method=method, id=transact_id)
+        return cls._format_log_file_path.format(directory=directory, transaction_id=transact_id)
 
     @classmethod
-    def _get_log_response_file_path(cls, method, directory, transact_id):
+    def _get_log_response_file_path(cls, directory, transact_id):
         """creates the final file path for response logs"""
-        return cls._format_log_file_path.format(directory=directory, method=method, id=transact_id)
+        return cls._format_log_file_path.format(directory=directory, transaction_id=transact_id)
+
+    @classmethod
+    def _get_next_id_from_counter(cls, id_frmt=None):
+        id_frmt = id_frmt or cls._format_counter_id
+        return id_frmt.format(id=cls.transaction_counter.next())
 
     @classmethod
     def _get_transaction_id_parts(cls):
         """gets the parts to include in the transaction id (log file name)"""
-        return [cls.transaction_counter.next()]
+        return [cls._get_next_id_from_counter()]
 
     @classmethod
     def _make_transaction_id(cls, *args):
@@ -88,8 +95,7 @@ class RestfulAPI(object):
         return '.'.join(map(str, filter(None, cls._get_transaction_id_parts() + list(args))))
 
     def make_transaction_id(self, *args):
-        arg_list = [self.base_url] + list(args)
-        return self._make_transaction_id(*arg_list)
+        return self._make_transaction_id(*args)
 
     @classmethod
     def _extract_log_kwargs(cls, **kwargs):
@@ -106,24 +112,22 @@ class RestfulAPI(object):
         :return:
         """
         transact_name = kwargs.pop('transact_name', None)
-        if transact_name:
-            transact_id = '{}.{}'.format(cls.transaction_counter.next(), transact_name)
-        else:
-            transact_id = kwargs.pop('transact_id', None) or cls.transaction_counter.next()
+        transact_parts = [transact_name, response.request.method]
+        transact_id = kwargs.pop('transact_id', None) or cls._make_transaction_id(*transact_parts)
         save_request = kwargs.pop('save_request', True)
         save_response = kwargs.pop('save_response', True)
         dir_request = kwargs.pop('dir_request', cls._cls_request_dir)
         dir_response = kwargs.pop('dir_response', cls._cls_response_dir)
         if save_request:
             try:
-                request_file_path = cls._get_log_request_file_path(response.request.method, dir_request, transact_id)
+                request_file_path = cls._get_log_request_file_path(dir_request, transact_id)
                 cls._log_request(response, request_file_path)
             except Exception as exc:
                 log.error('Exception logging rest transaction request: id={} exc={} trace...'.format(transact_id, exc),
                           exc_info=True)
         if save_response:
             try:
-                response_file_path = cls._get_log_response_file_path(response.request.method, dir_response, transact_id)
+                response_file_path = cls._get_log_response_file_path(dir_response, transact_id)
                 cls._log_response(response, response_file_path)
             except Exception as exc:
                 log.error('Exception logging rest transaction response: id={} exc={} trace...'.format(transact_id, exc),
@@ -263,3 +267,54 @@ class RestfulAPI(object):
         """sends a DELETE command using requests package"""
         self.__add_default_log_dirs_to_kwargs(kwargs)
         return self._request_delete(url, **kwargs)
+
+
+class JsonRestObject(RestfulAPI):
+    """
+    a class which is basically a restful interface with an embedded json structure
+    convenient for restful interfaces which communicate with json objects
+    """
+
+    _json_structure_class = JsonStructure
+
+    def __init__(self, json_data=None, base_url=None, **kwargs):
+        """
+        initialize a new JsonRestObject
+        :param base_url: base url of the restful api
+        :param json_data: any underlying data json structure
+        :param kwargs: kwargs for restful api
+        """
+        self.data = self._json_structure_class(json_data=json_data)
+        super(JsonRestObject, self).__init__(base_url=base_url, **kwargs)
+
+    def update_data(self, new_data):
+        """update the underlying data (json structure)"""
+        self.data.load_data(new_data)
+
+    def _update_with_response_content(self, response, force_update=False):
+        """use content from request.response to update this object"""
+        if response is None:
+            return
+        content = self._get_content_from_json(response)
+        if force_update or (content and isinstance(content, dict)):
+            self.update_data(content)
+
+    def _request_and_update(self, method, url, **kwargs):
+        """make a request to a URL, and then load the response into the underlying data json structure"""
+        assert method in [self.request_get, self.request_post, self.request_put, self.request_delete]
+        return_response = kwargs.pop('return_response', False)
+        force_update = kwargs.pop('force_update', False)
+        response = method(url, **kwargs)
+        self._update_with_response_content(response, force_update)
+        if return_response or response is None:
+            return response
+        return response.ok
+
+    def get_and_update(self, url, **kwargs):
+        """make a get request to URL, and then load response"""
+        return self._request_and_update(self.request_get, url, **kwargs)
+
+    def set_and_update(self, url, **kwargs):
+        """make a put request to URL with payload, and then load response"""
+        kwargs.setdefault('json', self.data.export_to_json_data())
+        return self._request_and_update(self.request_put, url, **kwargs)
