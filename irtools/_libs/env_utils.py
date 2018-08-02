24a3a6bfbd518b1a684e5d016681a2f22abc6f6a
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2018-08-02 13:45:36 +0300

    get mac address / addresses added to env utils

--- irtools/_libs/env_utils.py
+++ irtools/_libs/env_utils.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 
+# Standard Imports
+import uuid
+import re
+
 # irtools Imports
 from irtools import *
 
@@ -10,6 +14,72 @@ log = logging.getLogger('irtools.utils.env')
 def get_ip():
     """Gets the IP of this machine (if it can)"""
     return socket.gethostbyname(socket.gethostname())
+
+
+def get_mac():
+    """uses uuid lib to get mac"""
+    mac_int = uuid.getnode()
+    mac_str = _mac_int_to_str(mac_int)
+    return mac_str
+
+
+def _mac_int_to_str(mac_int):
+    mac_str = ':'.join(("%012X" % mac_int)[i:i + 2] for i in range(0, 12, 2))
+    return mac_str
+
+
+def get_macs():
+    """uses code found in uuid lib to get all mac addresses"""
+    if running_on_windows:
+        # Get the hardware address on Windows by running ipconfig.exe
+        macs = set()
+        dirs = ['', r'c:\windows\system32', r'c:\winnt\system32']
+        for directory in dirs:
+            try:
+                pipe = os.popen(os.path.join(directory, 'ipconfig') + ' /all')
+            except IOError:
+                continue
+            with pipe:
+                for line in pipe:
+                    value = line.split(':')[-1].strip().lower()
+                    if re.match('([0-9a-f][0-9a-f]-){5}[0-9a-f][0-9a-f]', value):
+                        macs.add(int(value.replace('-', ''), 16))
+        return map(_mac_int_to_str, macs)
+    elif running_on_linux:
+        # Get the hardware address on Unix by running ifconfig
+        macs = set()
+
+        def _find_macs(command, args, hw_identifiers, get_index):
+            try:
+                pipe = uuid._popen(command, args)
+                if not pipe:
+                    return
+                with pipe:
+                    for line in pipe:
+                        words = line.lower().rstrip().split()
+                        for i in range(len(words)):
+                            if words[i] in hw_identifiers:
+                                try:
+                                    word = words[get_index(i)]
+                                    mac = int(word.replace(':', ''), 16)
+                                    if mac:
+                                        macs.add(mac)
+                                except (ValueError, IndexError):
+                                    # Virtual interfaces, such as those provided by
+                                    # VPNs, do not have a colon-delimited MAC address
+                                    # as expected, but a 16-byte HWAddr separated by
+                                    # dashes. These should be ignored in favor of a
+                                    # real MAC address
+                                    pass
+            except IOError:
+                pass
+
+        # This works on Linux ('' or '-a'), Tru64 ('-av'), but not all Unixes.
+        for args in ('', '-a', '-av'):
+            _find_macs('ifconfig', args, ['hwaddr', 'ether'], lambda i: i + 1)
+        return map(_mac_int_to_str, macs)
+    else:
+        raise RuntimeError('unsupported platform')
 
 
 def get_env(key, default=None, as_bool=False, as_int=False, clean=True):
@@ -49,4 +119,4 @@ def get_env(key, default=None, as_bool=False, as_int=False, clean=True):
     return value
 
 
-__all__ = ['get_env', 'get_ip']
+__all__ = ['get_env', 'get_ip', 'get_mac', 'get_macs']
