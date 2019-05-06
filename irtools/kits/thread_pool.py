79c0345f17bf2e6360f1d5c59e96c7fdce2d41cc
Inbar Rose <5904674+InbarRose@users.noreply.github.com>
2019-05-06 14:14:04 +0300

    issue#55 add logging method to pool and send logs there issue#54 added progress reporting convenience

--- irtools/kits/thread_pool.py
+++ irtools/kits/thread_pool.py
@@ -3,6 +3,7 @@
 # Standard Imports
 from Queue import Queue
 from threading import Thread
+import time
 
 # irtools Imports
 from irtools import *
@@ -22,7 +23,9 @@ class Worker(Thread):
         self.parent_pool = parent_pool
         self.daemon = True
         self.start()
-        self.trace_logs = False
+
+    def notify(self, log_func, message, **kwargs):
+        self.parent_pool.worker_notification(self.worker_id, log_func, message, **kwargs)
 
     @property
     def operating(self):
@@ -35,16 +38,15 @@ class Worker(Thread):
         """
         while self.operating:
             func, args, kwargs = self.task_queue.get()
-            if self.trace_logs:
-                log.trace('worker starting task: worker-id={} func={}'.format(self.worker_id, func))
+            if self.parent_pool.trace_logs:
+                self.notify(log.trace, 'worker starting task', func=func)
             try:
                 func(*args, **kwargs)
             except Exception as exc:
-                log.error('Exception in thread: worker-id={} func={} exc={}'.format(self.worker_id, func, exc),
-                          exc_info=True)
-                self.parent_pool.task_nok(self.worker_id)
+                self.notify(log.error, 'worker exception', func=func, exc=exc)
+                self.parent_pool.task_nok(self.worker_id, func, args, kwargs)
             else:
-                self.parent_pool.task_ok(self.worker_id)
+                self.parent_pool.task_ok(self.worker_id, func, args, kwargs)
             finally:
                 # Mark this task as done, whether an exception happened or not
                 self.task_queue.task_done()
@@ -52,11 +54,14 @@ class Worker(Thread):
 
 class ThreadPool(object):
     """ Pool of threads consuming tasks from a queue """
-    def __init__(self, num_threads, worker_class=None, max_queue_size=0):
+    def __init__(self, num_threads, worker_class=None, max_queue_size=0, **kwargs):
         # task queue
         self.tasks = Queue(max_queue_size)
         # flags
         self._operating = True
+        # params
+        self.trace_logs = kwargs.pop('trace_logs', False)
+        self.name = kwargs.pop('name', id(self))
         # counters
         self._total_task_count = 0
         self._tasks_ok_count = 0
@@ -76,6 +81,20 @@ class ThreadPool(object):
         worker = self._worker_class(worker_id=worker_id, task_queue=self.tasks, parent_pool=self)
         self._workers[worker_id] = worker
         self.worker_counters[worker_id] = 0
+
+    @property
+    def identification(self):
+        return '{}({})'.format(self.__class__.__name__, self.name)
+
+    def worker_notification(self, worker_id, log_func, message, **kwargs):
+        msg = '{} {}({}) {}: {}'.format(
+            self.identification,
+            self._worker_class.__name__,
+            worker_id,
+            message,
+            ' '.join(sorted(utils.convert_dict_params_to_list_of_string(kwargs)))
+        )
+        log_func(msg)
 
     @property
     def operating(self):
@@ -120,12 +139,12 @@ class ThreadPool(object):
         """count the number of tasks each worker completes"""
         self.worker_counters[worker_id] += 1
 
-    def task_ok(self, worker_id):
+    def task_ok(self, worker_id, func, args, kwargs):
         """count the number of tasks completed okay"""
         self._increment_worker_count(worker_id)
         self._tasks_ok_count += 1
 
-    def task_nok(self, worker_id):
+    def task_nok(self, worker_id, func, args, kwargs):
         """count the number of tasks completed not okay"""
         self._increment_worker_count(worker_id)
         self._tasks_nok_count += 1
@@ -161,3 +180,19 @@ class ThreadPool(object):
     def wait_completion(self):
         """ Wait for completion of all the tasks in the queue BLOCKING """
         self.tasks.join()
+
+    def notify_progress(self):
+        """sends the current progress status to the log"""
+        log.info('{} progress: ran={}/{} ok={} nok={}'.format(
+            self.identification, self.count_completed, self.count_total, self.count_ok, self.count_nok))
+
+    def wait_with_progress(self, period=30, timeout=None):
+        log.info('{} waiting with progress: threads={} tasks={}'.format(
+            self.identification, len(self._workers), self.count_total))
+
+        while self.processing:
+            self.notify_progress()
+            time.sleep(period)
+
+        log.info('{} finished: ran={}/{} ok={} nok={}'.format(
+            self.identification, self.count_completed, self.count_total, self.count_ok, self.count_nok))
